f973cb08328ce9c65f1b8f3f66cbfa5decbeaa5b
Rohan Gope <rg2512@it.jgec.ac.in>
2023-09-02 22:37:51 +0530

    adding communities

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,11 +1,58 @@
-import React from 'react'
+import CommunityCard from "@/components/Cards/CommunityCard";
+import UserCard from "@/components/Cards/UserCard";
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import ThreadsTab from "@/components/shared/ThreadsTab";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { profileTabs } from "@/constants";
+import { fetchCommunities } from "@/lib/actions/community.actions";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import Image from "next/image";
+import { redirect } from "next/navigation";
 
-const page = async() => {
+async function Page(){
+  const user = await currentUser();
+
+  if(!user) return null;
+
+  const userInfo = await fetchUser(user.id)
+
+  if(!userInfo?.onboarded) redirect("/onboarding");
+  
+  // Fetch Communities
+  const result = await fetchCommunities({
+    searchString: '',
+    pageNumber: 1,
+    pageSize: 10,
+  })
+
   return (
     <section>
-        <h1 className='head-text mb-10'>Community</h1>
+        <h1 className='head-text mb-10'>Search</h1>
+
+        {/* Search Bar */}
+
+        <div className="mt-14 flex flex-col gap-9">
+        {result.communities.length === 0 ? (
+            <p className="no-result">No Communities</p>
+          ):(
+            <>
+              {result.communities.map((community)=>(
+                <CommunityCard
+                  key={community.id}
+                  id={community.id}
+                  name={community.name}
+                  username={community.username}
+                  imgUrl={community.image}
+                  bio={community.bio}
+                  members={community.members}
+                />
+              ))}
+            </>
+          )}
+        </div>
     </section>
   )
 }
 
-export default page
+export default Page
